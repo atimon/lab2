6ce912a5d14fff66494c2ed821bf5409e810c268
atimon <andresitotimon@hotmail.com>
2018-10-12 17:37:20 +0100

    Successful test

--- src/main/Lab2.java
+++ src/main/Lab2.java
@@ -9,8 +9,9 @@ public class Lab2 {
     // this method should calculate what percentage
     // the thisMany argument is of thatMany
     public float percentageOf(float thisMany, float thatMany) {
-        // TODO: implement
-        return 0.0f;
+        float result;
+        result = thisMany/thatMany*100;
+        return result;
     }
 
     // this method should calculate the overall module mark
@@ -19,16 +20,18 @@ public class Lab2 {
     // of 100) knowing that the weights of the components are
     // 15, 25, and 60 respectively.
     public float overallModuleMark(float cswk1, float cswk2, float exam) {
-        // TODO: implement
-        return 0.0f;
+        float result2;
+        result2 = ((cswk1/100)*15)+((cswk2/100)*25)+((exam/100)*60);
+        return result2;
     }
 
     // this method should calculate the required exam mark needed
     // to achieve a desired overall module mark, given the marks
     // for the first and second coursework.
     public float desiredExamMarkToAchieve(float cswk1, float cswk2, float moduleMark) {
-        // TODO: implement
-        return 0.0f;
+        float desired;
+        desired = ((100*moduleMark)/60)-((cswk1*15)/60)-((cswk2*25)/60);
+        return desired;
     }
 
     // this method should calculate the mark that is needed for
@@ -36,7 +39,10 @@ public class Lab2 {
     // first two components as well as the weights of all components and
     // the desired overall mark
     public float markToAchieve(float c1, float w1, float c2, float w2, float overall) {
-        // TODO: implement
-        return 0.0f;
+        float finalmark;
+        float w3;
+        w3 = 100-w1-w2;
+        finalmark = ((100*overall)/w3)-((c1*w1)/w3)-((c2*w2)/w3);
+        return finalmark;
     }
 }
